9b8291554128fdf6a749bb2e429be3f214b3e5fb
Marco Dijkslag <m.dijkslag@student.utwente.nl>
2021-01-04 22:24:31 +0100

    Initial Commit

---
+++ ds4macos/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ds4macos
+//
+//  Created by Marco Dijkslag on 04/01/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ds4macos/ds4macosApp.swift
@@ -0,0 +1,17 @@
+//
+//  ds4macosApp.swift
+//  ds4macos
+//
+//  Created by Marco Dijkslag on 04/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct ds4macosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
