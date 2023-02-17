da82d61445d92e9fa8f1f8b286523a36e9b1eeb6
Marco Dijkslag <9336858+marcowindt@users.noreply.github.com>
2023-02-17 23:53:33 +0100

    gitignore Pods

--- Pods/Target Support Files/Pods-ds4macos/Pods-ds4macos-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ds4macos : NSObject
-@end
-@implementation PodsDummy_Pods_ds4macos
-@end
--- Pods/Target Support Files/SwiftAsyncSocket/SwiftAsyncSocket-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SwiftAsyncSocket : NSObject
-@end
-@implementation PodsDummy_SwiftAsyncSocket
-@end
